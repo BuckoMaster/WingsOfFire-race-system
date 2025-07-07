9696d080a5773c955ee9ea775aedb3e359a4e6b5
BuckoMaster <vujadinkaranovic@gmail.com>
2025-07-07 15:18:58 +0000

    Ispravljen redosled rasa sa SandWing na pravom mestu

--- src/pages/Rase.js
+++ src/pages/Rase.js
@@ -3,31 +3,36 @@ import RaceCard from '../components/RaceCard';
 
 export default function Rase() {
   const races = [
-    {
-      name: 'MudWing / Blatokrili',
-      image: '/images/races/Mudwing-Blatokrili-menu.png',
-    },
-    {
-      name: 'RainWing / Kišokrili',
-      image: '/images/races/Rainwing-Kisokrili-menu.png',
-    },
-    {
-      name: 'SeaWing / Morskokrili',
-      image: '/images/races/Seawing-Morskokrili-menu.png',
-    },
-    {
-      name: 'NightWing / Noćnokrili',
-      image: '/images/races/Nightwing-Nocnokrili-menu.png',
-    },
-    {
-      name: 'SkyWing / Nebokrili',
-      image: '/images/races/Skywing-Nebokrili-menu.png',
-    },
-    {
-      name: 'IceWing / Ledenokrili',
-      image: '/images/races/Icewing-Ledenokrili-menu.png',
-    },
-  ];
+  {
+    name: 'MudWing / Blatokrili',
+    image: '/images/races/Mudwing-Blatokrili-menu.png',
+  },
+  {
+    name: 'SeaWing / Morskokrili',
+    image: '/images/races/Seawing-Morskokrili-menu.png',
+  },
+  {
+    name: 'RainWing / Kišokrili',
+    image: '/images/races/Rainwing-Kisokrili-menu.png',
+  },
+  {
+    name: 'NightWing / Noćnokrili',
+    image: '/images/races/Nightwing-Nocnokrili-menu.png',
+  },
+  {
+    name: 'SandWing / Peskokrili',
+    image: '/images/races/Sandwing-Peskokrili-menu.png',
+  },
+  {
+    name: 'SkyWing / Nebokrili',
+    image: '/images/races/Skywing-Nebokrili-menu.png',
+  },
+  {
+    name: 'IceWing / Ledenokrili',
+    image: '/images/races/Icewing-Ledenokrili-menu.png',
+  },
+];
+
 
   return (
     <div className="p-6">
